66e8a5cb4e041c955ba7d47dedea458b80fbcc2e
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-03 16:33:20 +0530

    Just playing around Yo :)

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText(){
+    return 'Click on Me Yo :-)';
+}
+
 // Create a React Component
 const App = () => {
-    return <div>Hi There Yo! :)</div>;
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+        </div>
+    );
 }
 
 // Take the React Component & show it on the screen.
