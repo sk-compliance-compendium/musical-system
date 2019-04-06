bdbeb8f3899dd1fc05179b625d3386728500d4b6
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-06 19:40:40 +0530

    Wrapping up Get Location Yo :]

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -19,10 +19,17 @@ class App extends React.Component {
     }
 
     render() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage }</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
         return (
-            <div>
-              Latitude: {this.state.lat} <br/>
-              Error: {this.state.errorMessage}
+            <div className="ui active inverted dimmer">
+                <div className="ui large text loader">Loading</div>
             </div>
         );
     }
