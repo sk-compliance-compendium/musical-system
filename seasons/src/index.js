4e83a209334a8adf9229c54121d04a27f45cb3c2
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-07 09:51:15 +0530

    Started working on Lifecycle Methods Brah :]

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loader from './Loader';
 class App extends React.Component {
     constructor(props){
         super(props);
@@ -27,11 +28,7 @@ class App extends React.Component {
             return <div>Latitude: {this.state.lat}</div>
         }
 
-        return (
-            <div className="ui active inverted dimmer">
-                <div className="ui large text loader">Loading</div>
-            </div>
-        );
+        return <Loader />;
     }
 }
 
