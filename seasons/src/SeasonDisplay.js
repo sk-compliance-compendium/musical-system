ebc9b3f6c17ad0f86483d8e868a4346b2cca0f31
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-08 14:59:07 +0530

    Getting the Season into our code adventure Brah!

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display Yo :)</div>
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach Bro!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, it is Chilly Brah!",
+        iconName: "snowflake"
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+        <div>
+            <i className = {`${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className = {`${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from './Loader';
-class App extends React.Component {
-    constructor(props){
-        super(props);
+import SeasonDisplay from './SeasonDisplay';
 
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
+class App extends React.Component {
+
+    state = {lat:null, errorMessage:''};
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
 
@@ -25,7 +20,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat= {this.state.lat} />
         }
 
         return <Loader />;
