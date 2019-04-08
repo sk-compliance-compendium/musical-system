6c25ff76c79ff6abbc49ddf301061a7bbf1a77fb
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-08 15:47:52 +0530

    Polishing up Get Seasons Bro! Its coming up Cool Yo!

--- seasons/src/Loader.js
+++ seasons/src/Loader.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 class Loader extends Component{
     render() {
         return (
-            <div className="ui active inverted dimmer">
-                <div className="ui large text loader">Loading</div>
+            <div className="ui active dimmer">
+                <div className="ui large text loader">{this.props.message}</div>
             </div>
         );
     }
 }
+
+Loader.defaultProps = {
+    message: 'Loading....'
+};
 
 export default Loader;
