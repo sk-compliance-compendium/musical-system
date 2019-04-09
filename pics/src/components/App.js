3a245fbce5a0937bdd458a92304556797ea05f80
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-09 15:17:56 +0530

    Getting my hands dirty on Unsplash API Yo!

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    async onSearchSubmit(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers:{
+                Authorization: 'Client-ID 1e1d81bf442f12ed735dea22c56332ae1a9d1955eb2d8120e3ac539228c6416b'
+            }
+        });
+
+        console.log(response.data.results);
     }
     render(){
         return (
